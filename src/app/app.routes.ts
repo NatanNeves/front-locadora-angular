1b4f7676bb02dc3ddc859cb4e5093b4c2d41701b
NatanNeves <dev.natanbarbosa@gmail.com>
2025-07-28 18:28:37 -0300

    feat: creating login screen, menu bar and adding styles with material bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { Component } from '@angular/core';
 import { LoginComponent } from './components/layouts/login/login.component';
 import { CarrosListComponent } from './components/carros/carros-list/carros-list.component';
 import { CarrosDetailsComponent } from './components/carros/carros-details/carros-details.component';
+import { MarcasListComponent } from './components/marcas/marcas-list/marcas-list.component';
+import { MarcasDetailsComponent } from './components/marcas/marcas-details/marcas-details.component';
 
 
 export const routes: Routes = [
@@ -12,6 +14,9 @@ export const routes: Routes = [
     {path: 'admin', component: PrincipalComponent, children: [
         {path:'carros', component: CarrosListComponent},
         {path:'carros/new', component: CarrosDetailsComponent},
-        {path:'carros/edit/:id', component: CarrosDetailsComponent}
+        {path:'carros/edit/:id', component: CarrosDetailsComponent},
+        {path:'marcas', component: MarcasListComponent},
+        {path:'marcas/new', component: MarcasDetailsComponent},
+        {path:'marcas/edit/:id', component: MarcasDetailsComponent}
     ]}
 ];
