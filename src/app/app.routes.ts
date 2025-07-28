222a7f5e8492b36b8efe2e0b19351f509ac18545
NatanNeves <dev.natanbarbosa@gmail.com>
2025-07-28 15:09:36 -0300

    feat: create components of layouts, carro and marca components, with models marca and carro, add routes for application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { PrincipalComponent } from './components/layouts/principal/principal.component';
+import { Component } from '@angular/core';
+import { LoginComponent } from './components/layouts/login/login.component';
+import { CarrosListComponent } from './components/carros/carros-list/carros-list.component';
+import { CarrosDetailsComponent } from './components/carros/carros-details/carros-details.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:'', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'login', component: LoginComponent},
+    {path: 'admin', component: PrincipalComponent, children: [
+        {path:'carros', component: CarrosListComponent},
+        {path:'carros/new', component: CarrosDetailsComponent},
+        {path:'carros/edit/:id', component: CarrosDetailsComponent}
+    ]}
+];
---
+++ src/app/components/carros/carros-list/carros-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carros-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './carros-list.component.html',
+  styleUrl: './carros-list.component.scss'
+})
+export class CarrosListComponent {
+
+}
