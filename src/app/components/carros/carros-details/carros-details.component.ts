d27269d0958487f7054eb1982cdb78628870fa41
NatanNeves <dev.natanbarbosa@gmail.com>
2025-07-28 20:37:34 -0300

    feat: creating car list, button loggout and form of create car

--- src/app/components/carros/carros-details/carros-details.component.ts
+++ src/app/components/carros/carros-details/carros-details.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { Carro } from '../../../models/carro';
 
 @Component({
   selector: 'app-carros-details',
   standalone: true,
-  imports: [],
+  imports: [MdbFormsModule, FormsModule],
   templateUrl: './carros-details.component.html',
   styleUrl: './carros-details.component.scss'
 })
 export class CarrosDetailsComponent {
+  carro: Carro=(new Carro(0, ""));
 
+  save(){
+    alert("Carro salvo com sucesso!");
+  }
 }
--- src/app/components/carros/carros-list/carros-list.component.ts
+++ src/app/components/carros/carros-list/carros-list.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { Carro } from '../../../models/carro';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-carros-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './carros-list.component.html',
   styleUrl: './carros-list.component.scss'
 })
 export class CarrosListComponent {
 
+  id!: number;
+  nome!: string;
+
+  lista: Carro []=[];
+
+  constructor(){
+    this.lista.push(new Carro(1, 'Fusca'));
+    this.lista.push(new Carro(2, 'Civic'));
+    this.lista.push(new Carro(3, 'Corolla'));
+    this.lista.push(new Carro(4, 'Onix'));
+  }
+
+  editar(){
+
+  }
+
+  excluir(){
+
+  }
 }
